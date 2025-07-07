af75a6d211990c4bc11d05121c92cdb07602d8da
Labib Mahmud <54614860+streamsniperrty@users.noreply.github.com>
2025-07-06 20:41:34 -0400

    Add files via upload

---
+++ .config/nvim/init.lua
@@ -0,0 +1,17 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+vim.call('plug#begin')
+
+Plug('morhetz/gruvbox')
+Plug('kyazdani42/nvim-tree.lua')
+Plug('kyazdani42/nvim-web-devicons')
+
+vim.call('plug#end')
+
+home = os.getenv("HOME")
+package.path = home .. "/.config/nvim/?.lua;" .. package.path
+
+require "common"
+require "theme"
+-- dofile("theme.lua")
+require "vimtree"
